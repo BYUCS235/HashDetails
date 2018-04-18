621272b01c727587bcf8c40ff704b48267fc382a
Mark Clement <clement@cs.byu.edu>
2018-04-17 20:03:09 -0600

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -3,15 +3,15 @@
 #include <string>
 using namespace std;
 
-template <typename T>
-int hashme(T param, int tablesize) {
-    unsigned char *ptr = (unsigned char *)&param;
+
+int hashme(string param, int tablesize) {
+    unsigned char *ptr = (unsigned char *)param.c_str();
     int sum = 0;
-    for(int i = 0; i < sizeof(param); i++) {
+    for(int i = 0; i < param.size(); i++) {
         sum += ptr[i];
     }
     int hashval = sum%tablesize;
-    cout << "Size = "<<sizeof(param)<<" Sum = "<<sum<<" hash "<<hashval<<endl;
+    cout << "Size = "<<param.size()<<" Sum = "<<sum<<" hash "<<hashval<<endl;
     return hashval;
 }
 int main()
@@ -19,11 +19,7 @@ int main()
     const int tablesize = 11;
     vector<string> strvec;
     string foo("Hello");
-    cout << "Hash of Hello "<<hashme<string>(foo, tablesize)<<endl;
-    strvec.push_back(foo);
-    cout << "Hash of vector "<<hashme<vector<string>>(strvec, tablesize)<<endl;
-    foo = "Goodbye";
-    cout << "Hash of Goodbye "<<hashme<string>(foo, tablesize)<<endl;
-    strvec.push_back(foo);
-    cout << "Hash of vector "<<hashme<vector<string>>(strvec, tablesize)<<endl;
-}
+    cout << "Hash of Hello "<<hashme(foo, tablesize)<<endl;
+    string foo2("Hello");
+    cout << "Hash2 of Hello "<<hashme(foo2, tablesize)<<endl;
+    }
